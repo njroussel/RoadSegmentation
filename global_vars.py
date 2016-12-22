492cc5ddc97c139767ba3fa63eec397cba2dab44
Nicolas Roussel <n.roussel13@gmail.com>
2016-12-22 21:31:28 +0100

    PARAMS

--- global_vars.py
+++ global_vars.py
@@ -3,31 +3,31 @@ PIXEL_DEPTH = 255
 NUM_LABELS = 2
 TRAINING_SIZE = 100
 SEED = 43212  # Set to None for random seed.
-EVAL_BATCH_SIZE = 64  # 64
-BATCH_SIZE = 64
+EVAL_BATCH_SIZE = 128  # 64
+BATCH_SIZE = 128
 NUM_EPOCHS = 100 # Will later be determined by validation.
-HSV = True
-ROTATE_IMAGES = False
-ROTATED_IMG = 100
-RESTORE_MODEL = False  # If True, restore existing model instead of training a new one
-POST_PROCESS = False
+HSV = False
+ROTATE_IMAGES = True
+ROTATED_IMG = 300
+RESTORE_MODEL = True  # If True, restore existing model instead of training a new one
+POST_PROCESS = True
 TEST_PREDICTIONS = True
 RECORDING_STEP = 1000
 LEARNING_RATE = 0.001
-OPTI_F1 = False
+OPTI_F1 = True
 FILTER_SIZE = 3
 
 KEEP_DROPOUT = 0.8
 
 # Convolution network architecture
-CONV_ARCH = [1, 1] # The best architecture so far on validation.
+CONV_ARCH = [2, 4, 4, 4] # The best architecture so far on validation.
 # [2, 4, 4, 6] Best on test set.
 # We will keep both, but mostly use [2, 2, 4] because of our limited
 # computation power.
 CONV_DEPTH = [32, 64, 128, 256]
 
-FC_ARCH = 1
-FC_DEPTH = [512]
+FC_ARCH = 2
+FC_DEPTH = [1024, 1024]
 
 # Set image patch size in pixels
 # IMG_PATCH_SIZE should be a multiple of 4
@@ -35,7 +35,7 @@ FC_DEPTH = [512]
 IMG_PATCH_SIZE = 16
 
 # Border for enhanced context.
-IMG_BORDER = 16  # Will be set after archi is found
+IMG_BORDER = 24  # Will be set after archi is found
 
 IMG_TOTAL_SIZE = IMG_PATCH_SIZE + 2 * IMG_BORDER
 
--- global_vars_pp.py
+++ global_vars_pp.py
@@ -3,20 +3,23 @@ NUM_LABELS = 2
 TRAINING_SIZE = 100
 SEED = 43212  # Set to None for random seed.
 EVAL_BATCH_SIZE = 128  # 64
-BATCH_SIZE =  64
-NUM_EPOCHS = 3 # Will later be determined by validation.
-ROTATED_IMG = 50
+BATCH_SIZE =  128
+NUM_EPOCHS = 10 # Will later be determined by validation.
+ROTATED_IMG = 300
 ROTATE_IMAGES = True
 RESTORE_MODEL = False  # If True, restore existing model instead of training a new one
 TEST_PREDICTIONS = True
 RECORDING_STEP = 1000
 LEARNING_RATE = 0.001
 
+FILTER_SIZE = 3
 INPUT_PATCH_SIZE = 16  # MUST BE THE SAME AS PATCH SIZE IN GLOBAL_VARS
 
-# Convolution network architecture
-CONV_ARCH = [1]
+OPTI_F1 = True
 
+# Convolution network architecture
+CONV_ARCH = [2]
+KEEP_DROPOUT = 0.8
 CONV_DEPTH = [32, 64, 128, 256]
 
 FC_ARCH = 2
@@ -27,7 +30,7 @@ FC_DEPTH = [1024, 1024]
 IMG_PATCH_SIZE = 2  # MUST BE EVEN
 
 # Border for enhanced context.
-IMG_BORDER = 15  # MUST BE EVEN (could eventually be even ....)
+IMG_BORDER = 30  # MUST BE EVEN (could eventually be even ....)
 
 IMG_TOTAL_SIZE = IMG_PATCH_SIZE + 2 * IMG_BORDER
 
@@ -36,8 +39,8 @@ THETA_THRESH = 0.01
 
 # Validation parameters:
 VALIDATION_TRAIN_PERC = 0.6
-VALIDATION_VAL_PERC = 0.3
-VALIDATION_TEST_PERC = 0.1
+VALIDATION_VAL_PERC = 0.2
+VALIDATION_TEST_PERC = 0.2
 
 # Hyperparameters validation
 COMPUTE_VALIDATION_F1_SCORE_FOR_EACH_EPOCH = True
